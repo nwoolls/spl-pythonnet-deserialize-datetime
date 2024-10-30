e0e40f3923f5d594a67f779dbfa2a89268d4a10c
Nate Woolls <nathanial.woolls@idmworks.com>
2024-10-30 14:46:13 -0400

    Add pythonnet package and DateTime deserialization logic
    Added a reference to the pythonnet package in the .csproj file to enable interaction with Python runtime. Implemented logic in Program.cs to run Python code that fetches the current datetime and attempts to convert it to C# DateTime object, including error handling for conversion failures.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using Python.Runtime;
+
+PythonEngine.Initialize();
+try
+{
+    using (Py.GIL())
+    {
+        const string sourceCode = """
+                                  import datetime
+                                  today = datetime.datetime.now()
+                                  """;
+        var scope = Py.CreateScope();
+        scope.Exec(sourceCode);
+        var today = scope.Get("today");
+        Console.WriteLine($"Type: {today.GetPythonType().Name}");
+
+        try
+        {
+            Console.WriteLine(today.ToDateTime(CultureInfo.InvariantCulture));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+
+        try
+        {
+            Console.WriteLine(today.As<DateTime>());
+        }
+        catch (Exception ex) {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
+finally
+{
+    RuntimeData.FormatterType = typeof(NoopFormatter);
+    PythonEngine.Shutdown();
+}
